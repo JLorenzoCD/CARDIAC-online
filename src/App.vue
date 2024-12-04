4565e65da0d5a1f57c2643af89683849e74f7b7d
lorenzo.canovas <jlorenzocanovas@gmail.com>
2024-12-04 19:19:53 -0300

    Init repo

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div class="container mx-auto pt-3">
+		<h1 class="text-3xl font-bold underline mb-8">CARDIAC Online</h1>
+	</div>
+</template>
+
+<style scoped></style>
