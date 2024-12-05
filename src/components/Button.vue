510bbe48ef6822d36b7fe7a167d3637046658fa6
lorenzo.canovas <jlorenzocanovas@gmail.com>
2024-12-05 15:03:18 -0300

    feat: :sparkles: Componente botton

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useAttrs } from 'vue';
+
+// defineProps<{
+// 	customClass?: string;
+// }>();
+
+// Obtenemos los atributos que no se declaran como props
+const attrs = useAttrs();
+</script>
+
+<template>
+	<button
+		v-bind="attrs"
+		class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+	>
+		<slot />
+	</button>
+</template>
