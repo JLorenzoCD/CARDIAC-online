58cdde8ab0a6552e44ce52aedc65ac573f859fbc
lorenzo.canovas <jlorenzocanovas@gmail.com>
2024-12-05 21:11:02 -0300

    feat: :sparkles: Componente Cell, el cual muestra el indice y contenido para las tarjetas de entrada, salida y de memoria

---
+++ src/components/Cell.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+defineProps<{
+	content: string;
+	index: number;
+}>();
+</script>
+
+<template>
+	<li class="border border-black h-6 w-[92px] text-center flex justify-between">
+		<span class="bg-gray-200 w-7 border-r border-black text-xs p-1">{{ index.toString().padStart(2, '0') }}</span>
+		<p class="w-16">{{ content }}</p>
+	</li>
+</template>
