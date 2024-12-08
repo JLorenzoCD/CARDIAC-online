680bb2d2472af430666d93166912be92ee851250
lorenzo.canovas <jlorenzocanovas@gmail.com>
2024-12-08 19:13:10 -0300

    fix: :lipstick: Modificando componente Cell, en caso de que el contenido sea '000', se deja el espacio en  blanco para evitar tener mucha info en pantalla

--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -11,6 +11,6 @@ defineProps<{
 <template>
 	<li class="border border-black h-6 w-[92px] text-center flex justify-between">
 		<span class="bg-gray-200 w-7 border-r border-black text-xs p-1">{{ index.toString().padStart(2, '0') }}</span>
-		<p :class="{ 'w-16': true, 'bg-emerald-200': isCurr }">{{ content }}</p>
+		<p :class="{ 'w-16': true, 'bg-emerald-200': isCurr }">{{ content == '000' ? '' : content }}</p>
 	</li>
 </template>
